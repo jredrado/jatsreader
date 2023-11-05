c4ad15fe8ab69eebd8271882776484a604ee9c5a
jredrado <jesus.redrado@gmail.com>
2023-11-05 11:51:17 +0000

    Custom resolvers

--- src/slight/streamer/static/reader/prelude.js
+++ src/slight/streamer/static/reader/prelude.js
@@ -1,5 +1,24 @@
 var resolve_urs = function (urs_link){
-    api_url = 'https://3001-jredrado-rusttemplate-8sepdmxsit1.ws-eu105.gitpod.io/pub/' + urs_link + '/resolve';
+    api_url = '/resolve/' + urs_link ;
+
+    $.getJSON(api_url, function(data) {
+            // Suponiendo que el API devuelve un objeto con una propiedad 'message'
+            //let message = data.message;
+            console.log(data);
+            if (data.length > 0) {
+                Swal.fire('Cita verificada', data[0].endpoint, 'success');
+            }else {
+                Swal.fire('Cita', "No se ha podido resolver el enlace");
+            }
+
+        })
+        .fail(function() {
+            Swal.fire('Cita', "No se ha podido resolver el enlace");
+        });
+}
+
+var resolve_urs_locator = function (urs_link){
+    api_url = '/locate/' + urs_link ;
 
     $.getJSON(api_url, function(data) {
             // Suponiendo que el API devuelve un objeto con una propiedad 'message'
